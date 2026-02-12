0a07442a27f89191527a87abaf68f9b9a1db38fa
Vahid V. <vhid.vz@gmail.com>
2026-02-12 15:38:59 +0330

    update

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2026-present Vahid V. <vhid.vz@gmail.com>
-#
-# SPDX-License-Identifier: Apache-2.0
---
+++ tests/wenex/common/collection_test.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+
+from wenex.common import COLLECTION, COLL
+
+
+def test_COLLECTION():
+    assert COLLECTION("grants", "auth") == "auth/grants"
+
+    # Unset db test
+    with pytest.raises(Exception):
+        COLLECTION("grants")
+
+    # Optional db test
+    os.environ.update({"PROTOTYPING_APP": "AUTH"})
+    assert COLLECTION("grants") == "auth/grants"
+
+    # Invalid app test
+    os.environ.update({"PROTOTYPING_APP": "AUTHS"})
+    with pytest.raises(Exception):
+        COLLECTION("grants")
+
+
+def test_COLL():
+    coll, db = COLL("auth/grants")
+    assert coll == "grants" and db == "auth"
