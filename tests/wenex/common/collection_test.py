737cc5b2d01b0c0ea0080785bde220b376d267e1
Vahid V. <vhid.vz@gmail.com>
2026-02-14 10:44:21 +0330

    update

--- tests/wenex/common/collection_test.py
+++ tests/wenex/common/collection_test.py
@@ -5,22 +5,22 @@ from wenex.common.collection import COLLECTION, COLL
 
 
 def test_COLLECTION():
-    assert COLLECTION("grants", "auth") == "auth/grants"
+  assert COLLECTION("grants", "auth") == "auth/grants"
 
-    # Unset db test
-    with pytest.raises(Exception):
-        COLLECTION("grants")
+  # Unset db test
+  with pytest.raises(Exception):
+    COLLECTION("grants")
 
-    # Optional db test
-    os.environ.update({"PROTOTYPING_APP": "AUTH"})
-    assert COLLECTION("grants") == "auth/grants"
+  # Optional db test
+  os.environ.update({"PROTOTYPING_APP": "AUTH"})
+  assert COLLECTION("grants") == "auth/grants"
 
-    # Invalid app test
-    os.environ.update({"PROTOTYPING_APP": "AUTHS"})
-    with pytest.raises(Exception):
-        COLLECTION("grants")
+  # Invalid app test
+  os.environ.update({"PROTOTYPING_APP": "AUTHS"})
+  with pytest.raises(Exception):
+    COLLECTION("grants")
 
 
 def test_COLL():
-    coll, db = COLL("auth/grants")
-    assert coll == "grants" and db == "auth"
+  coll, db = COLL("auth/grants")
+  assert coll == "grants" and db == "auth"
